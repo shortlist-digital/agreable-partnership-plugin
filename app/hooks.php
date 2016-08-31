d0112d5df3ff1dd492937c6766055810dafcdfec
dan_adeyemi <iamdeaneyelid@gmail.com>
2016-08-31 15:53:55 +0100

    adding related articles hook to partnerships plugin

--- app/hooks.php
+++ app/hooks.php
@@ -5,7 +5,11 @@
 use AgreablePartnershipPlugin\Hooks\BasicDetailsAcf;
 use AgreablePartnershipPlugin\Hooks\SocialMediaAcf;
 use AgreablePartnershipPlugin\Hooks\HtmlOverridesAcf;
+use AgreablePartnershipPlugin\Hooks\RelatedContentAcf;
+
 
 (new BasicDetailsAcf)->init();
 (new SocialMediaAcf)->init();
 (new HtmlOverridesAcf)->init();
+(new RelatedContentAcf)->init();
+
